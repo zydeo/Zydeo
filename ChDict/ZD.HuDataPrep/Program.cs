43416409975cded5d94637a292656cfdf2e605e9
ug <gabor.ugray@kilgray.com>
2015-01-06 19:00:22 +0100

    Code reorg: ChDict stuff into subfolder

---
+++ ChDict/ZD.HuDataPrep/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ZD.HuDataPrep
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 4)
+            {
+                Console.WriteLine("Takes these arguments:\r\n");
+                Console.WriteLine("1: Lexical scope file\r\n");
+                Console.WriteLine("2: Wikipedia page titles file\r\n");
+                Console.WriteLine("3: CEDICT file\r\n");
+                Console.WriteLine("4: HanDeDict file\r\n");
+                Console.WriteLine("5: Output file: Lexical backbone XML\r\n");
+                Console.WriteLine("6: Output file: Summary statistics\r\n");
+                if (Debugger.IsAttached) Console.ReadLine();
+                return -1;
+            }
+
+            StreamReader srScope = null;
+            StreamReader srWiki = null;
+            StreamReader srCedict = null;
+            StreamReader srHanDeDict = null;
+            StreamWriter swOut = null;
+            StreamWriter swStats = null;
+            try
+            {
+                srScope = new StreamReader(args[0]);
+                srWiki = new StreamReader(args[1]);
+                srCedict = new StreamReader(args[2]);
+                srHanDeDict = new StreamReader(args[3]);
+                swOut = new StreamWriter(args[4]);
+                swStats = new StreamWriter(args[5]);
+                WrkHuData wrk = new WrkHuData(srScope, srWiki, srCedict, srHanDeDict);
+                wrk.Work();
+                wrk.Finish(swOut, swStats);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                if (Debugger.IsAttached) Console.ReadLine();
+                return -1;
+            }
+            finally
+            {
+                if (srScope != null) srScope.Dispose();
+                if (srWiki != null) srWiki.Dispose();
+                if (srCedict != null) srCedict.Dispose();
+                if (srHanDeDict != null) srHanDeDict.Dispose();
+                if (swOut != null) swOut.Dispose();
+                if (swStats != null) swStats.Dispose();
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ ChDict/ZD.HuDataPrep/WrkHuData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ZD.HuDataPrep
+{
+    internal class WrkHuData
+    {
+        private readonly StreamReader srScope;
+        private readonly StreamReader srWiki;
+        private readonly StreamReader srCedict;
+        private readonly StreamReader srHanDeDict;
+
+        public WrkHuData(StreamReader srScope, StreamReader srWiki, StreamReader srCedict, StreamReader srHanDeDict)
+        {
+            this.srScope = srScope;
+            this.srWiki = srWiki;
+            this.srCedict = srCedict;
+            this.srHanDeDict = srHanDeDict;
+        }
+
+        public void Work()
+        {
+        }
+
+        public void Finish(StreamWriter swOut, StreamWriter swStats)
+        {
+
+        }
+    }
+}
