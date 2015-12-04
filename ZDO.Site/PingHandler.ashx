ad42979ec3e506235665f8c2bf62f6508eb46e82
ug <gabor.ugray@kilgray.com>
2015-12-04 19:03:29 +0100

    dedicated ping keepalive handler

---
+++ ZDO.Site/PingHandler.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="PingHandler.ashx.cs" Class="Site.PingHandler" %>
---
+++ ZDO.Site/PingHandler.ashx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Site
+{
+    public class PingHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("pong");
+        }
+
+        public bool IsReusable { get { return false; } }
+    }
+}
