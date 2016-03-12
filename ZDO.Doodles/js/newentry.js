c8f6ba9f722bd7e8b6074062c68dd0f05f865d00
ug <gabor.ugray@kilgray.com>
2016-03-12 21:22:34 +0100

    CHDICT doodle: new entry editor

---
+++ ZDO.Doodles/js/newentry.js
@@ -0,0 +1,394 @@
+var zdNewEntry = (function() {
+  var template =
+    '<div class="formBlock active" id="blockSimp">' +
+    '  <div class="formBlockLabel">Egyszerűsített</div>' +
+    '  <div class="formBlockFrame">' +
+    '    <input id="newEntrySimp" maxlength="8" readonly/>' +
+    '    <div class="formButtonRight accept" id="acceptSimp">' +
+    '      <img src="static/sign.svg" alt=""/>' +
+    '    </div>' +
+    '    <div class="formButtonRight edit" id="editSimp">' +
+    '      <img src="static/draw.svg" alt=""/>' +
+    '    </div>' +
+    '    <div class="formNote" id="noteSimp">' +
+    '      A folytatáshoz kattints a zöld <i>Jóváhagyás</i> gombra, vagy üss Entert.' +
+    '    </div>' +
+    '    <div class="formErrors" id="errorsSimp">' +
+    '      Az automatikus ellenőrzés az alábbi problémákat találta.' +
+    '      Kérlek, korrigáld ezeket, majd kattints ismét a zöld <i>Jóváhagyás</i> gombra.' +
+    '      <ul id="errorListSimp"></ul>' +
+    '    </div>' +
+    '  </div>' +
+    '</div>' +
+    '<div class="formBlock future" id="blockTrad">' +
+    '  <div class="formBlockLabel">Hagyományos</div>' +
+    '  <div class="formBlockFrame">' +
+    '    <div id="newEntryTradCtrl">' +
+    '      &nbsp;' +
+    '    </div>' +
+    '    <div class="formButtonRight accept" id="acceptTrad">' +
+    '      <img src="static/sign.svg" alt=""/>' +
+    '    </div>' +
+    '    <div class="formButtonRight edit" id="editTrad">' +
+    '      <img src="static/draw.svg" alt=""/>' +
+    '    </div>' +
+    '    <div class="formNote">' +
+    '      Ha nem a megfelelő hagyományos írásjegy áll az első helyen, a kívánt elemre' +
+    '      kattintva helyesbítheted. Ha kész, kattints a zöld <i>Jóváhagyás</i> gombra.' +
+    '    </div>' +
+    '  </div>' +
+    '</div>' +
+    '<div class="formBlock future" id="blockPinyin">' +
+    '  <div class="formBlockLabel">Pinyin</div>' +
+    '  <div class="formBlockFrame">' +
+    '    <div id="newEntryPinyinCtrl">' +
+    '      &nbsp;' +
+    '    </div>' +
+    '    <div class="formButtonRight accept" id="acceptPinyin">' +
+    '      <img src="static/sign.svg" alt=""/>' +
+    '    </div>' +
+    '    <div class="formButtonRight edit" id="editPinyin">' +
+    '      <img src="static/draw.svg" alt=""/>' +
+    '    </div>' +
+    '    <div class="formNote" id="notePinyin">' +
+    '      Ellenőrizd a szótagok pinyin-átiratát. Ha a szó szerepel a CEDICT-ben vagy a HanDeDict-ben,' +
+    '      akkor az ezekből ismert olvasat áll az első helyen, amúgy az egyes írásjegyek leggyakoribb olvasata.' +
+    '      Ha kész, kattints a zöld <i>Jóváhagyás</i> gombra.' +
+    '    </div>' +
+    '    <div class="formErrors" id="errorsPinyin">' +
+    '      Duplikátum: ilyen címszó már létezik (megegyező egyszerűsített és hagyományos írásjegyek,' +
+    '      azonos kiejtéssel).' +
+    '    </div>' +
+    '  </div>' +
+    '</div>' +
+    '<div class="formBlock future" id="blockTrg">' +
+    '  <div class="formBlockLabel">Magyar</div>' +
+    '  <div class="formBlockFrame">' +
+    '    <textarea id="newEntryTrg" maxlength="1024" readonly></textarea>' +
+    '    <div class="formButtonRight accept" id="acceptTrg">' +
+    '      <img src="static/sign.svg" alt=""/>' +
+    '    </div>' +
+    '    <div class="formButtonRight edit" id="editTrg">' +
+    '      <img src="static/draw.svg" alt=""/>' +
+    '    </div>' +
+    '    <div class="formNote" id="noteTrg">' +
+    '      Add meg, újsorokkal elválasztva, a címszó magyar jelentéseit.' +
+    '      Ha kész, kattints a zöld <i>Jóváhagyás</i> gombra.' +
+    '    </div>' +
+    '    <div class="formErrors" id="errorsTrg">' +
+    '      Az automatikus ellenőrzés az alábbi problémákat találta.' +
+    '      Kérlek, korrigáld ezeket, majd kattints ismét a zöld <i>Jóváhagyás</i> gombra.' +
+    '      <ul id="errorListTrg"></ul>' +
+    '    </div>' +
+    '  </div>' +
+    '</div>' +
+    '<div class="formBlock future hidden" id="blockReview">' +
+    '  <div class="formBlockLabel">Előnézet</div>' +
+    '  <div class="formBlockFrame">' +
+    '    <div id="newEntryRender"></div>' +
+    '    <input id="newEntryNote" maxlength="128" placeholder="Megjegyzés, forrásmegjelölés" readonly/>' +
+    '    <div class="formErrors" id="errorsReview">' +
+    '      Kérlek, fűzz hozzá egy rövid megjegyzést vagy forrásmegjelölést.' +
+    '    </div>' +
+    '    <div class="formSubmit" id="newEntrySubmit">Eltárolom</div>' +
+    '  </div>' +
+    '</div>';
+
+  var server;
+
+  function documentReady() {
+    $("#newEntrySimp").bind("input", onSimpChanged);
+    $("#acceptSimp").click(onSimpAccept);
+    $("#editSimp").click(onSimpEdit);
+    $("#acceptTrad").click(onTradAccept);
+    $("#editTrad").click(onTradEdit);
+    $("#acceptPinyin").click(onPinyinAccept);
+    $("#editPinyin").click(onPinyinEdit);
+    $("#acceptTrg").click(onTrgAccept);
+    $("#editTrg").click(onTrgEdit);
+    $("#newEntrySubmit").click(onSubmit);
+
+    $("#newEntrySimp").prop("readonly", false);
+    $("#newEntrySimp").focus();
+  }
+
+  $(document).ready(function () {
+    documentReady();
+  });
+
+  function setActive(block) {
+    $(".formBlock").removeClass("active");
+    $(".formBlock").removeClass("ready");
+    $(".formBlock").removeClass("future");
+    $("#blockReview").addClass("hidden");
+
+    $("#newEntrySimp").prop("readonly", true);
+    $("#newEntryTrg").prop("readonly", true);
+    $("#newEntryNote").prop("readonly", true);
+    $(".formErrors").removeClass("visible");
+    $(".formNote").removeClass("hidden");
+    if (block == "simp") {
+      $("#newEntrySimp").prop("readonly", false);
+      $("#newEntrySimp").focus();
+      $("#blockSimp").addClass("active");
+      $("#blockTrad").addClass("future");
+      $("#blockPinyin").addClass("future");
+      $("#blockTrg").addClass("future");
+      $("#blockReview").addClass("future");
+    }
+    else if (block == "trad") {
+      $("#blockSimp").addClass("ready");
+      $("#blockTrad").addClass("active");
+      $("#blockPinyin").addClass("future");
+      $("#blockTrg").addClass("future");
+      $("#blockReview").addClass("future");
+    }
+    else if (block == "pinyin") {
+      $("#blockSimp").addClass("ready");
+      $("#blockTrad").addClass("ready");
+      $("#blockPinyin").addClass("active");
+      $("#blockTrg").addClass("future");
+      $("#blockReview").addClass("future");
+    }
+    else if (block == "trg") {
+      $("#newEntryTrg").prop("readonly", false);
+      $("#newEntryTrg").focus();
+      $("#blockSimp").addClass("ready");
+      $("#blockTrad").addClass("ready");
+      $("#blockPinyin").addClass("ready");
+      $("#blockTrg").addClass("active");
+      $("#blockReview").addClass("future");
+    }
+    else if (block == "review") {
+      $("#blockReview").removeClass("hidden");
+      $("#newEntryNote").prop("readonly", false);
+      $("#newEntryNote").focus();
+      $("#blockSimp").addClass("ready");
+      $("#blockTrad").addClass("ready");
+      $("#blockPinyin").addClass("ready");
+      $("#blockTrg").addClass("ready");
+      $("#blockReview").addClass("active");
+    }
+  }
+
+  function onSubmit(evt) {
+    if ($("#newEntryNote").val().length < 6) {
+      $("#errorsReview").addClass("visible");
+      $("#newEntryNote").focus();
+    }
+    else {
+      $("#errorsReview").removeClass("visible");
+    }
+  }
+
+  function onTrgEdit(evt) {
+    setActive("trg");
+  }
+
+  function onTrgAccept(evt) {
+    if ($("#acceptTrg").hasClass("disabled")) return;
+    server.verifyTrg($("#newEntryTrg").val(), onTrgVerified);
+    $("#acceptTrg").addClass("disabled");
+  }
+
+  function onTrgVerified(res) {
+    $("#acceptTrg").removeClass("disabled");
+    if (!res.passed) {
+      $("#errorsTrg").addClass("visible");
+      $("#noteTrg").addClass("hidden");
+      $("#errorListTrg").empty();
+      for (var i = 0; i < res.errors.length; ++i) {
+        var liErr = $('<li/>');
+        liErr.text(res.errors[i]);
+        $("#errorListTrg").append(liErr);
+      }
+      $("#newEntryTrg").focus();
+    }
+    else {
+      $("#errorsTrg").removeClass("visible");
+      $("#noteTrg").removeClass("hidden");
+      $("#newEntryRender").html(res.preview);
+      setActive("review");
+    }
+  }
+
+  function onPinyinAccept(evt) {
+    if ($("#acceptPinyin").hasClass("disabled")) return;
+    server.verifyHead($("#newEntrySimp").val(), getTrad(), getPinyin(), onHeadVerified);
+    $("#acceptPinyin").addClass("disabled");
+  }
+
+  function onHeadVerified(res) {
+    $("#acceptPinyin").removeClass("disabled");
+    if (!res.passed) {
+      $("#errorsPinyin").addClass("visible");
+      $("#notePinyin").addClass("hidden");
+    }
+    else {
+      $("#errorsPinyin").removeClass("visible");
+      $("#notePinyin").removeClass("hidden");
+      setActive("trg");
+    }
+  }
+
+  function onPinyinEdit(evt) {
+    setActive("pinyin");
+  }
+
+  function onTradEdit(evt) {
+    setActive("trad");
+  }
+
+  function onTradAccept(evt) {
+    if ($("#acceptTrad").hasClass("disabled")) return;
+    setActive("pinyin");
+  }
+
+  function onSimpChanged(evt) {
+    server.processSimp($("#newEntrySimp").val(), onSimpProcessed);
+  }
+
+  function onSimpProcessed(trad, pinyin) {
+    $("#newEntryTradCtrl").empty();
+    for (var  i = 0; i < trad.length; ++i) {
+      var tpos = $('<div class="newEntryTradPos"/>');
+      for (var j = 0; j < trad[i].length; ++j) {
+        var tspan = $('<span />');
+        if (j != 0) tspan.addClass("tradAlt");
+        tspan.text(trad[i][j]);
+        tpos.append(tspan);
+      }
+      $("#newEntryTradCtrl").append(tpos);
+    }
+    if (trad.length == 0) $("#newEntryTradCtrl").append('\xA0');
+    $(".tradAlt").unbind("click", onTradAltClicked);
+    $(".tradAlt").click(onTradAltClicked);
+
+    updatePinyin(pinyin);
+  }
+
+  function onSimpAccept(evt) {
+    if ($("#acceptSimp").hasClass("disabled")) return;
+    server.verifySimp($("#newEntrySimp").val(), onSimpVerified);
+    $("#acceptSimp").addClass("disabled");
+  }
+
+  function onSimpVerified(res) {
+    $("#acceptSimp").removeClass("disabled");
+    if (!res.passed) {
+      $("#errorsSimp").addClass("visible");
+      $("#noteSimp").addClass("hidden");
+      $("#errorListSimp").empty();
+      for (var i = 0; i < res.errors.length; ++i) {
+        var liErr = $('<li/>');
+        liErr.text(res.errors[i]);
+        $("#errorListSimp").append(liErr);
+      }
+      $("#newEntrySimp").focus();
+    }
+    else {
+      $("#errorsSimp").removeClass("visible");
+      $("#noteSimp").removeClass("hidden");
+      setActive("trad");
+    }
+  }
+
+  function onSimpEdit(evt) {
+    setActive("simp");
+  }
+
+  function getTrad() {
+    var res = "";
+    var tctrl = $("#newEntryTradCtrl");
+    tctrl.children().each(function() {
+      res += $(this).children().first().text();
+    });
+    return res;
+  }
+
+  function getPinyin() {
+    var res = "";
+    var tctrl = $("#newEntryPinyinCtrl");
+    tctrl.children().each(function() {
+      res += $(this).children().first().text();
+    });
+    return res;
+  }
+
+  function onTradAltClicked(evt) {
+    if (!$("#blockTrad").hasClass("active")) return;
+
+    var parent = $(this).parent();
+    var tchars = [];
+    tchars.push($(this).text());
+    parent.children().each(function() {
+      if ($(this).text() != tchars[0])
+        tchars.push($(this).text());
+    });
+    parent.empty();
+    for (var i = 0; i < tchars.length; ++i) {
+      var tspan = $('<span />');
+      if (i != 0) tspan.addClass("tradAlt");
+      tspan.text(tchars[i]);
+      parent.append(tspan);
+    }
+    $(".tradAlt").unbind("click", onTradAltClicked);
+    $(".tradAlt").click(onTradAltClicked);
+
+    server.processSimpTrad($("#newEntrySimp").val(), getTrad(), onSimpTradProcessed);
+  }
+
+  function updatePinyin(pinyin) {
+    $("#newEntryPinyinCtrl").empty();
+    for (var i = 0; i != pinyin.length; ++i) {
+      var ppos = $('<div class="newEntryPinyinPos"/>');
+      for (j = 0; j != pinyin[i].length; ++j) {
+        var pspan = $('<span/>');
+        if (j != 0) pspan.addClass("pyAlt");
+        pspan.text(pinyin[i][j]);
+        ppos.append(pspan);
+      }
+      $("#newEntryPinyinCtrl").append(ppos);
+    }
+    if (pinyin.length == 0) $("#newEntryPinyinCtrl").append('\xA0');
+    $(".pyAlt").unbind("click", onPyAltClicked);
+    $(".pyAlt").click(onPyAltClicked);
+  }
+
+  function onPyAltClicked(evt) {
+    if (!$("#blockPinyin").hasClass("active")) return;
+
+    var parent = $(this).parent();
+    var tsylls = [];
+    tsylls.push($(this).text());
+    parent.children().each(function() {
+      if ($(this).text() != tsylls[0])
+        tsylls.push($(this).text());
+    });
+    parent.empty();
+    for (var i = 0; i < tsylls.length; ++i) {
+      var tspan = $('<span />');
+      if (i != 0) tspan.addClass("pyAlt");
+      tspan.text(tsylls[i]);
+      parent.append(tspan);
+    }
+    $(".pyAlt").unbind("click", onPyAltClicked);
+    $(".pyAlt").click(onPyAltClicked);
+  }
+
+  function onSimpTradProcessed(pinyin) {
+    updatePinyin(pinyin);
+  }
+
+  return {
+    init: function(id) {
+      $(id).html(template);
+    },
+    setServer: function(srv) {
+      server = srv;
+    }
+  }
+})();
+
+zdNewEntry.init("#newEntry");
+zdNewEntry.setServer(zdNewEntryShim);
