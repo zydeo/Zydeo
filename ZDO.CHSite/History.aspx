5766cfbd02278739c8f89a82f5b8faf2e7094bcd
ug <gabor.ugray@kilgray.com>
2016-04-01 09:43:43 +0200

    history display - html doodle

---
+++ ZDO.CHSite/History.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="History.aspx.cs"
+  Inherits="ZDO.CHSite.History" ClientIDMode="Static" %>
+<%@ MasterType VirtualPath="~/Site.master" %>
+
+<asp:Content ID="mainContent" ContentPlaceHolderID="mainContentPlaceholder" runat="server">
+
+<div id="content">
+<asp:Literal runat="server" ID="lit" />
+</div>
+
+</asp:Content>
---
+++ ZDO.CHSite/History.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace ZDO.CHSite
+{
+    public partial class History : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Add CSS files
+            Master.AddCss("style.css");
+            Master.AddCss("history.css");
+            Master.AddCss("entry.css");
+
+            // Render static content
+            string path = HttpRuntime.AppDomainAppPath;
+            path = Path.Combine(path, "Content");
+            path = Path.Combine(path, "HistoryDoodle.html");
+            using (StreamReader sr = new StreamReader(path))
+            {
+                lit.Text = sr.ReadToEnd();
+            }
+        }
+    }
+}
